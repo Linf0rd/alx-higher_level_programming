4fa9ccabea6273c637496f44c589f95aff46c326
Linf0rd <linfordsnagger@gmx.com>
2023-08-14 17:18:59 +0200

    Print matrix of integers using format method
    Adding a Python script that demonstrates printing a matrix of integers using the 'format' method. The script defines a function 'print_matrix_integer' that takes a list of lists (matrix) of integers as an argument. It uses nested loops to iterate through the rows and columns of the matrix, utilizing the 'enumerate' function to track the current index. The 'format' method is used to print each integer in a formatted manner.

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -3,6 +3,6 @@ def print_matrix_integer(matrix=[[]]):
     for row in matrix:
         for x, num in enumerate(row):
             if x != len(row) - 1:
-                print("{}".format(num), end=" ")
+                print("{:d}".format(num), end=" ")
             else:
-                print("{}".format(num))
+                print("{:d}".format(num))
