5513bf2724fbebc8c144919045eeb88a9002617b
Linf0rd <linfordsnagger@gmx.com>
2023-08-29 16:25:53 +0200

    Implement safe_print_list function
    The 'safe_print_list' function prints elements from the input list up to the specified index 'x', handling IndexErrors by catching exceptions and continuing execution. It utilizes a 'try' block to iterate over the list and print elements, incrementing the count of printed elements. If an IndexError occurs, it's caught by the 'except' block, and the loop continues to execute. Finally, the function prints a newline and returns the count of printed elements. This ensures that the function gracefully handles potential index errors and provides accurate count information.

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -3,8 +3,8 @@
 def safe_print_list(my_list=[], x=0):
     count = 0
     try:
-        for l in range(x):
-            print(my_list[l], end="")
+        for i in range(x):
+            print(my_list[i], end="")
             count += 1
     except IndexError:
         pass
