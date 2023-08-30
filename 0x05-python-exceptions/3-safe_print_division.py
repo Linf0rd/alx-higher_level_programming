39ff5f3f21cf6c27d66aff27a4cf0e1b6a0b5a9c
Linf0rd <linfordsnagger@gmx.com>
2023-08-30 20:38:36 +0200

    Implement safe_print_division function
    The 'safe_print_division' function calculates the division of two numbers while handling a 'ZeroDivisionError' gracefully. It prints the result within a 'finally' block before returning it. This function showcases a safe way to perform division and display the result even in exceptional cases.

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -2,9 +2,9 @@
 
 def safe_print_division(a, b):
     try:
-        result a / b
-    except (ZeroDivisionError, TypeError):
+        result = a / b
+    except ZeroDivisionError:
         result = None
     finally:
         print("Inside result: {}".format(result))
-        return result
+    return result
