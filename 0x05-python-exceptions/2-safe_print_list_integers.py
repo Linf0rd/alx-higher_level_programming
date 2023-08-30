b96036595d2ceba90343e16c7616c36dfe81efdd
Linf0rd <linfordsnagger@gmx.com>
2023-08-30 03:25:11 +0200

    Implement safe_print_list_integers function
    The 'safe_print_list_integers' function iterates through a list and prints the integers found within the specified range. It handles exceptions for index errors, ensuring that the loop doesn't continue beyond the list's length. This function demonstrates how to safely print only integers from a list.

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -7,6 +7,8 @@ def safe_print_list_integers(my_list=[], x=0):
             if type(my_list[i]) is int:
                 print("{:d}".format(my_list[i]), end="")
                 count += 1
+                for x in range(count):
+                    print("", end="")
         except IndexError:
             print("", end="")
         print()
