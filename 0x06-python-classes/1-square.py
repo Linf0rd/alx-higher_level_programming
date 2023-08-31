7d2996fb809284a41a068c09598e17fc0ef9ef96
Linf0rd <linfordsnagger@gmx.com>
2023-08-31 21:21:05 +0200

    Create Square class with __init__ method
    Defined the 'Square' class with an '__init__' method that initializes an instance with a 'size' attribute. The 'size' is passed as an argument during instance creation and is stored as a private attribute '__size'.

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+Class Square that defines a square.
+"""
+
+
+class Square:
+    """
+    Class Square that defines a sqaure.
+    """
+
+    def __init__(self, size):
+        """
+        Initializes a Square instance.
+
+        Args:
+            size (int): The size of the square.
+
+        """
+        self.__size = size
