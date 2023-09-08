90c9d8ee629cf95e7254e94a23a50d728db8c4d6
Linf0rd <linfordsnagger@gmx.com>
2023-09-08 16:46:27 +0200

    Implement unittest for 'max_integer' function
    Created a unittest script 'test_max_integer' that tests the 'max_integer' function with various input scenarios, including positive and negative integers, an empty list, a list with a single element, a list with duplicate elements, and a list with float values. The tests validate the correctness of the 'max_integer' function.

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -13,33 +13,51 @@ class TestMaxInteger(unittest.TestCase):
 
     """
     def test_max_integer(self):
-        # Test with a list of positive integers
-        self.assertEqual(max_integer([1, 2, 3, 4]), 4)
+        """ Test with a list of positive integers. """
+        positive = [1, 2, 3, 4]
+        self.assertEqual(max_integer(positive), 4)
 
-        # Test with a list of positive integers in descending order
-        self.assertEqual(max_integer([4, 3, 2, 1]), 4)
+    def test_positive_descending(self):
+        """ Test with a list of positive integers in descending order. """
+        positive_descending = [4, 3, 2, 1]
+        self.assertEqual(max_integer(positive_descending), 4)
 
-        # Test with a list of negative integers
-        self.assertEqual(max_integer([-1, -2, -3, -4]), -1)
+    def test_negative_integer(self):
+        """ Test with a list of negative integers """
+        negative = [-1, -2, -3, -4]
+        self.assertEqual(max_integer(negative), -1)
 
-        # Test with a list of negative integers in descending order
-        self.assertEqual(max_integer([-4, -3, -2, -1]), -1)
+    def test_negative_descending(self):
+        """ Test with a list of negative integers in descending order. """
+        negative_descending = [-4, -3, -2, -1]
+        self.assertEqual(max_integer(negative_descending), -1)
 
-        # Test with a list of positive and negative integers
-        self.assertEqual(max_integer([-1, 2, -3, 4]), 4)
+    def test_positive_and_negative(self):
+        """ Test with a list of positive and negative integers. """
+        positive_and_negative = [-1, 2, -3, 4]
+        self.assertEqual(max_integer(positive_and_negative), 4)
 
-        # Test with an empty list
-        self.assertIsNone(max_integer([]))
+    def test_empty_list(self):
+        """ Test with an empty list. """
+        empty = []
+        self.assertIsNone(max_integer(empty), None)
 
-        # Test with a list containing only one element
-        self.assertEqual(max_integer([5]), 5)
+        
+    def test_one_element(self):
+        """ Test with a list containing only one element. """
+        one_element = [5]
+        self.assertEqual(max_integer(one_element), 5)
 
-        # Test with a list containing duplicate elements
-        self.assertEqual(max_integer([3, 3, 3, 3]), 3)
 
-        # Test with a list containing float values
-        self.assertEqual(max_integer([1.5, 2.7, 3.9]), 3.9)
+    def test_duplicate_elements(self):
+        """ Test with a list containing duplicate elements. """
+        duplicate = [3, 3, 3, 3]
+        self.assertEqual(max_integer(duplicate), 3)
+
+    def test_floats(self):
+        """ Test with a list containing float values. """
+        floats = [1.5, 2.7, 3.9]
+        self.assertEqual(max_integer(floats), 3.9)
 
 if __name__ == '__main__':
     unittest.main()
-
