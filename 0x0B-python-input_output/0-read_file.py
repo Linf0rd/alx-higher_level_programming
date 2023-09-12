38c5cc98de4f89afdfd658afccf1745c37be7891
Linf0rd <linfordsnagger@gmx.com>
2023-09-12 18:09:52 +0200

    Create read_file function
    Defined the 'read_file' function, which reads the contents of a text file and prints it to the standard output.

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+A text file-reading function.
+"""
+
+def read_file(filename=""):
+    """
+    Prints the contents of a text file to stdout.
+    """
+    with open(filename, encoding='utf-8') as file:
+        print(file.read(), end="")
