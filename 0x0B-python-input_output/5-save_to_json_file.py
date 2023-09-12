416ea9143a4b76941f2093635ce3017404c7640d
Linf0rd <linfordsnagger@gmx.com>
2023-09-12 18:46:33 +0200

    Create save_to_json_file function
    Defined the 'save_to_json_file' function, which writes a Python object to a JSON file using json.dump().

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -9,5 +9,5 @@ def save_to_json_file(my_obj, filename):
     """
     Write an object to a text file using JSON
     """
-    wih open(filename, mode='w', encoding='utf-8') as file:
+    with open(filename, mode='w', encoding='utf-8') as file:
         json.dump(my_obj, file)
