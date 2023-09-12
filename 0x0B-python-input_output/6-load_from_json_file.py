5e92f948f0252dfe355f00fc1c7ec3c1d4f7ef07
Linf0rd <linfordsnagger@gmx.com>
2023-09-12 18:52:39 +0200

    Create load_from_json_file function
    Defined the 'load_from_json_file' function, which loads a Python object from a JSON file using json.load().

---
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+Define a JSON file-reading function.
+"""
+import json
+
+
+def load_from_json_file(filename):
+    """
+    Create a Python object from a JSON file.
+    """
+    with open(filename, mode='r', encoding='utf-8') as file:
+        return json.load(file)
