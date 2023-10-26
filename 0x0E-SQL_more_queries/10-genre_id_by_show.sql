108892f0b309d985f57385b3a8772c09c84590fd
Linf0rd <linfordsnagger@gmx.com>
2023-10-26 06:31:11 +0200

    List shows with at least one genre linked
    Added SQL script to list all TV shows from the  table along with their corresponding genre IDs from the  table, ordered by TV show title and genre ID

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -1,6 +1,5 @@
--- Script that lists all cities contained in the database hbtn_0d_usa
-
-SELECT cities.id, cities.name, states.name
-FROM cities, states
-WHERE cities.state_id = states.id
-ORDER BY cities.id ASC;
+-- List shows with at least one genre linked
+SELECT tv_shows.title, tv_show_genres.genre_id
+FROM tv_shows
+JOIN tv_show_genres ON tv_shows.id = tv_show_genres.tv_show_id
+ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
