425a4c9b6d14bee7d9050a18adc0a99251e4417c
Linf0rd <linfordsnagger@gmx.com>
2023-11-14 16:42:36 +0200

    Find the second largest number
    Implemented a Node.js script that takes command-line arguments, converts them to numbers, and finds the second largest number among them. If there are fewer than two arguments, it prints 0. The script uses the 'Array.sort()' method to sort the numbers in descending order and then prints the second element.

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,26 +1,10 @@
 #!/usr/bin/node
 
-function secondBiggest (args) {
-  if (args.length <= 2) {
-    return 0;
-  }
+const args = process.argv.slice(2).map(Number);
 
-  let largest = Number.MIN_SAFE_INTEGER;
-  let secondLargest = Number.MIN_SAFE_INTEGER;
-
-  for (let i = 0; i < args.length; i++) {
-    const num = parseInt(args[i]);
-    if (num > largest) {
-      secondLargest = largest;
-      largest = num;
-    } else if (num > secondLargest && num < largest) {
-      secondLargest = num;
-    }
-  }
-
-  return secondLargest;
+if (args.length <= 1) {
+  console.log(0);
+} else {
+  const sortedArgs = args.sort((a, b) => b - a);
+  console.log(sortedArgs[1]);
 }
-
-const args = process.argv.slice(2);
-
-console.log(secondBiggest(args));
