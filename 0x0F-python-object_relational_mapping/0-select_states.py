44fd2cd08dd990ac2b95d8665698d8a29588216e
Linf0rd <linfordsnagger@gmx.com>
2023-11-18 03:07:01 +0200

    Added script to list all states from the database hbtn_0e_0_usa
    Created a Python script that connects to the database hbtn_0e_0_usa and retrieves a list of all states. The script prompts the user for the database username, password, and name before connecting to the database and executing the SQL query to retrieve the states. The retrieved states are then printed to the console.

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -12,16 +12,13 @@ def main():
     Main function
     """
 
-    username = input("Enter username: ")
-    password = input("Enter password: ")
-    database = input("Enter database name: ")
 
     db = MySQLdb.connect(
         host="localhost",
         port=3306,
-        user=username,
-        passwd=password,
-        db=database,
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        db=sys.argv[3],
     )
 
     cursor = db.cursor()
