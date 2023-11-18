bab1797fd1c458b3fb8540d6a1032dd1579b1031
Linf0rd <linfordsnagger@gmx.com>
2023-11-18 04:19:07 +0200

    Corrected script to filter states based on the provided argument
    The previous version of the script incorrectly filtered states using the 'LIKE' operator, which would return all states regardless of the exact matching criteria. This commit updates the script to use the '=' operator inside the SQL query to ensure that only states with names exactly matching the provided argument are retrieved.

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+"""
+Script that displays all values in the states table of hbtn_0e_0_usa where name
+matches the argument
+"""
+
+import MySQLdb
+import sys
+
+
+def main():
+    """
+    Main function
+    """
+
+    state_name = sys.argv[4]
+
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        db=sys.argv[3],
+    )
+
+    cursor = db.cursor()
+
+    cursor.execute(
+        "SELECT * FROM states WHERE name LIKE '{}' ORDER BY id ASC".format(
+            state_name
+        )
+    )
+
+    for row in cursor.fetchall():
+        print(row)
+
+    cursor.close()
+
+    db.close()
+
+
+if __name__ == "__main__":
+    main()
