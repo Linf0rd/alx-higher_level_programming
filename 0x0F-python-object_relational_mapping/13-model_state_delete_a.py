4d71619448efdb6953f4e1c987b64101b3b8e392
Linf0rd <linfordsnagger@gmx.com>
2023-11-18 17:35:06 +0200

    Implement script to delete State objects with names containing 'a' from hbtn_0e_6_usa database
    Utilize SQLAlchemy and sessionmaker to establish a database connection and manage data modifications
    
    Effectively retrieve all State objects with names containing 'a' using SQL LIKE operator
    
    Iterate through the retrieved State objects and delete each one using session.delete()
    
    Commit the changes to the database using session.commit()
    
    Close the database session upon completion of data deletions

---
+++ 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+"""
+Script that deletes all State objects with a name containing the letter 'a'
+from the database hbtn_0e_6_usa
+"""
+
+
+import sys
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+
+
+def main():
+    """
+    Main function
+    """
+
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+
+    engine = create_engine(
+        'mysql+mysqldb://{}:{}@localhost/{}'.format(
+            username, password, database
+        ),
+        pool_pre_ping=True
+    )
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    states = session.query(State).filter(State.name.like('%a%')).all()
+
+    for state in states:
+        session.delete(state)
+
+    session.commit()
+
+    session.close()
+
+
+if __name__ == "__main__":
+    main()
