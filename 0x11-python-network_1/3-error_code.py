80eba1ac4eb67c4e1f9e5ab2b7b28badcf1440b4
Linf0rd <linfordsnagger@gmx.com>
2023-12-02 04:10:31 +0200

    Added script to fetch and display URL content with error handling
    Implemented a Python script that fetches the content from a specified URL and displays the decoded UTF-8 content. The script utilizes the urllib.request module to make an HTTP request to the URL. It also incorporates error handling to gracefully handle HTTP errors by catching urllib.error.HTTPError exceptions and displaying the corresponding error code.

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -16,5 +16,5 @@ if __name__ == "__main__":
         with urllib.request.urlopen(url) as response:
             body = response.read().decode('utf-8')
             print(body)
-        except urllib.error.HTTPError as e:
+    except urllib.error.HTTPError as e:
             print(f"Error code: {e.code}")
